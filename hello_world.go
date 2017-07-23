b47f5faf15285e5b484dc4c5b10b40442d8c6fe5
drozdovmobile <drozdov.mobile@gmail.com>
2017-07-23 16:34:35 +0300

    this is 1st hello_world commit

--- hello_world.go
+++ hello_world.go
@@ -6,10 +6,10 @@ import "fmt"
 func main() {
 
 	fmt.Println("hello world")
-	fmt.Println("___________")
-	fmt.Println("how are you&")
-	//this line  is new
-	fmt.Println("some new line ______=>")
+
+
+///
+
 
 }
 
